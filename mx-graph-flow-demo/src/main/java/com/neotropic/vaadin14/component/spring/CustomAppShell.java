2200cb2324bc8186eb584d99cae6a52ac67ae4d3
jcamachoe <julian.camacho@kuwaiba.org>
2025-10-01 11:59:41 -0500

    Initial commit: Snapshot of codebase from Subversion [(7629)] (https://sourceforge.net/p/kuwaiba/code/HEAD/tree/components/mxgraph-flow/branches/version-24.6.2/)

---
+++ mx-graph-flow-demo/src/main/java/com/neotropic/vaadin14/component/spring/CustomAppShell.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.neotropic.vaadin14.component.spring;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.server.PWA;
+
+@Push
+@PWA(name = "Project Base for Vaadin Flow with Spring", shortName = "Project Base")
+public class CustomAppShell implements AppShellConfigurator {
+    
+}
